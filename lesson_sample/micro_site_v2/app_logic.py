16906d8b6edf516f381ae09d28466541b0c8c508
djangouser <kmm2459304@stu.o-hara.ac.jp>
2025-09-25 11:47:13 +0900

    commkit

--- lesson_sample/micro_site_v2/app_logic.py
+++ lesson_sample/micro_site_v2/app_logic.py
@@ -61,6 +61,14 @@ def set_first_value(first_value) -> None:
     with open(ROBODOG_FILE, 'w', encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
+def get_addition() -> int:
+    """足し算の結果を返す"""
+    with open(ROBODOG_FILE, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    addition = data ['frist_value'] + data['second_value']
+
+    return addition
 
 # set_first_value: first_valueをjsonに保存
 # set_second_value: second_valueをjsonに保存
